bdd12588f805e475bb634f42c374b847b30f4366
Daniel Moss <dmoss@stratoswp.com>
2018-03-25 22:39:44 -0400

    initial commit and file structure

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require('path');
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8081;
+
+// serve static content
+app.use(express.static(path.join(__dirname, "public")));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./routes/htmlRoute')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
